7d6499a35d9c330d468420a2d5ab2bae09984d93
hartnetl <laura.hartnett@outlook.com>
2021-06-10 16:30:09 +0000

    Feat: Add empty core functions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Assign variables to be able to target textbox and option button text
+
+let scenarioText = document.getElementsByTagName('p');
+let optionButtons = document.getElementById('buttons-container');
+
+// Create funtion to start the game
+
+function runGame() {
+    // The intro message needs to display in text section
+    // One option button needs to display with text "Take me to the zoo"
+    console.log("you want to run the game")
+};
+
+function displayScenarioX() {
+    // This function needs to control what scenario to display
+    // And the associated options
+    console.log("you want to display the scenario text")
+};
+
+function optionResponse() {
+    // This function implements response based on the option you choose
+    console.log("you picked option X and so the following things must happen")
+};
+
+
