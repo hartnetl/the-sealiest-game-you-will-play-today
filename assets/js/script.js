989dd9823e4b7520d35e34063a18c918a0d7df1f
hartnetl <laura.hartnett@outlook.com>
2021-06-15 10:03:08 +0000

    Feat: Move scenarios back to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
- // Assign variables to be able to target textbox and option button text
+// Assign variables to be able to target textbox and option button text
 
 let scenarioText = document.getElementById('text-box');
 let optionButtons = document.getElementById('buttons-container');
@@ -69,6 +69,56 @@ function addLoss() {
     document.getElementById('lose').innerText = ++oldLoss;
 }
 
+// Create an array to hold all of the scenarios as objects containing the body text and option text
+
+let scenarios = [
+    {
+        id: 'intro',
+        message: "Picture this. It's a beautiful sunny day. You have no plans or responsibilities. Life is good. You decide to go to the zoo. What could go wrong?", 
+        response: [
+            {
+                option: "Take me to the zoo!",
+                goTo: 1
+            },
+        ],
+    },
+    {
+        id: 1,
+        message: `You get to the zoo and the queue to get in is really long. <br> Do you:`,
+        response: [
+            {
+                option: "Wait in line",
+                goTo: 2
+            },
+            {
+                option: "Queues are for nerds. Lets sneak in.",
+                goTo: 3
+            },
+        ],
+    },
+    {
+        id: 2,
+        message: "Welcome to the your end",
+        response: [
+            {
+                option: "I need to start again",
+                outcome: "lose",
+                goTo: 'intro'
+            },
+        ],
+    },
+    {
+        id: 'final',
+        message: "congratulations, you survived your day at the zoo!",
+        response: [
+            {
+                option: "Let's play again",
+                outcome: "win",
+                goTo: "intro"
+            },
+        ],
+    },
+];
 
 runGame();
 
