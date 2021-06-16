49a92f87be5608b680cb16c9876ef8586c6f324c
hartnetl <laura.hartnett@outlook.com>
2021-06-16 11:33:32 +0000

    Feat: Remove 'myDataKey' example from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -480,7 +480,7 @@ function populateStorage() {
     localStorage.setItem('losses', document.getElementById('lose').innerText);
 };
 
-localStorage.removeItem('ted');
+localStorage.removeItem('myDataKey');
 
 
 
